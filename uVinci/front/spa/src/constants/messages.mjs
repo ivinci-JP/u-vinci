245127118f8f80c0c008f50b84ca29c8c23bef88
iVinci-YusukeAra <yusuke.ara@i-vinci.co.jp>
2024-05-15 12:33:43 +0900

    使用していないステータスコードを削除

--- uVinci/front/spa/src/constants/messages.mjs
+++ uVinci/front/spa/src/constants/messages.mjs
@@ -1,9 +1,5 @@
 const messages = {
-  OK: "OK",
-  NO_CONTENT: "No Content",
-  BAD_REQUEST: "Bad Request",
-  REQUEST_TIMEOUT: "Request Timeout",
-  INTERNAL_SERVER_ERROR: "Internal Server Error"
+  OK: "OK"
 };
 
 export default messages;
